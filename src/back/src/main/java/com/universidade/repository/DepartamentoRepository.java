54dbdd1bb10e190105323cea299f4173000ecf21
fiori_daniel <daniel.fiori.souza@uel.br>
2025-10-17 09:58:41 -0300

    add repository interface for every entity

---
+++ src/back/src/main/java/com/universidade/repository/DepartamentoRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.universidade.repository;
+
+import com.universidade.entity.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+    
+}
---
+++ src/back/src/main/java/com/universidade/repository/DisciplinaRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.universidade.repository;
+
+import com.universidade.entity.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+    
+}
